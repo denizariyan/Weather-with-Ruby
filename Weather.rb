1b8c5d362497db7d8fcc4472bac8d95c82ecf734
Deniz Ariyan <30367397+denizariyan@users.noreply.github.com>
2018-02-18 04:36:45 +0100

    Forgot to delete the placeholder API key

--- Weather.rb
+++ Weather.rb
@@ -3,5 +3,5 @@ require 'rest-client'
 puts " "
 print "Lutfen bir sehir ismi girin:"
 sehir = gets.chomp
-data = JSON.parse(RestClient.get("http://api.openweathermap.org/data/2.5/weather?q=#{sehir}&units=metric&appid=f70cdd972aa5faa5167173d3fb70f166&lang=tr").body)
+data = JSON.parse(RestClient.get("http://api.openweathermap.org/data/2.5/weather?q=#{sehir}&units=metric&appid=YOUR_API_KEY&lang=tr").body)
 puts  "Hava Durumu Suanda: #{data["weather"][0]["description"]}"
